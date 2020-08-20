ff41315f88bef8e7918fe420bd9749d60602a4c2
pranav peddi <41251294+pranavpeddi@users.noreply.github.com>
2020-08-20 15:37:18 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todolist';
+  todos:string[]=["eat","sleep"];
+
+  onClick(value:string)
+  {
+      this.todos.push(value);
+  }
+
+  delete(i:number)
+  {
+    this.todos.splice(i,1);
+  }
 }
